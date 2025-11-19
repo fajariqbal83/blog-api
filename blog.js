8caf34da787e94a495de9d359a6efeaed0cb8120
fajariqbal83 <nazirshazia177@gmail.com>
2025-11-19 19:23:15 +0500

    add

--- blog.js
+++ blog.js
@@ -1,18 +1,70 @@
+async function loadPosts() {
+  let loadUrl =
+    "https://wordpress-1471720-5962383.cloudwaysapps.com/wp-json/custom/v1/blog/";
 
-async function loadBlogs() {
-  let loadUrl = "https://wordpress-1471720-5962383.cloudwaysapps.com/wp-json/custom/v1/blog/";  
-  
-  const urlResponse = await fetch(loadUrl); 
-  
-   console.log(urlResponse); 
-  
-  const data = await urlResponse.json();     
+  const urlResponse = await fetch(loadUrl); //calling the website
 
-  console.log(data);                          
+  const data = await urlResponse.json(); //converting it to real JavaScript data
+
+  return data; //only way to send the result out of function
 }
 
-loadBlogs();   
+const postPerPage = 6;
+let currentPage = 1;
 
+async function showPosts(page, currentPage) {
+  const data = await loadPosts();
 
+  console.log(data);
 
+  const start = (page - 1) * postPerPage;
+  const end = start + postPerPage;
+  const paginatedPosts = data.posts.slice(start, end);
 
+  let displayData = "";
+
+  paginatedPosts.forEach((element) => {
+    displayData += `
+    
+    <div style="display: flex; flex-direction: column;  border-radius: 8px; solid #000; padding: 10px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);">
+    <img style="width:300px" src="${element.featured_image}" alt="">
+    <div style="font-weight: bold; font-size: large;">${element.title}</div>
+      <div>${element.date}</div>
+      </div>
+      
+    `;
+  });
+
+  document.querySelector(".js-blog-display").innerHTML = displayData;
+}
+
+async function setupPagination(postPerPage, currentPage) {
+  const data = await loadPosts();
+
+  console.log(data);
+
+  const totalPages = Math.ceil(data.posts.length / postPerPage);
+
+  console.log(totalPages);
+
+  let buttonHtml = "";
+
+  for (let i = 1; i <= totalPages; i++) {
+    buttonHtml += `<button style="margin-right: 5px;font-size: 18px;" class="js-page-button" data-page="${i}">${i}</button>`;
+  }
+
+  const displayButton = (document.querySelector(".js-pagination").innerHTML =
+    buttonHtml);
+
+  document.querySelectorAll(".js-page-button").forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      const page = Number(e.target.dataset.page);
+      currentPage = page;
+      showPosts(currentPage);
+    });
+  });
+}
+
+showPosts(currentPage);
+
+setupPagination(postPerPage, currentPage);
